c7cefd3c3dad10b9413badafe671d7213bcad13f
bunnystring <ziknaf_548@hotmail.com>
2025-09-21 20:53:06 -0500

    Add infra-devices-service with project basic server configuration

---
+++ src/main/java/com/infragest/infra_devices_service/InfraDevicesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_devices_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraDevicesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraDevicesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infragest/infra_devices_service/InfraDevicesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infragest.infra_devices_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraDevicesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
